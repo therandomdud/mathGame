52e643a887a62b387fac38526302bd764d50f6d7
therandomdud <mail@mymail.com>
2023-01-07 23:37:46 +0100

    add mathGame and inputCorrect methode

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,3 @@
-import java.util.Locale;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -9,7 +8,7 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         while (runner) {
-            System.out.println("Type start to yes or no to quit");
+            System.out.println("Type yes to start or no to quit");
             String userInput = scanner.next();
             if (userInput.equalsIgnoreCase("yes")) {
                 mathGame();
@@ -23,11 +22,49 @@ public class Main {
     }
 
     public static void mathGame () {
+        Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("To quit type quit");
+        int num1,num2;
+        while (true) {
+            num1 = random.nextInt(20);
+            num2 = random.nextInt(20);
+            System.out.println(num1 + " + " + num2);
+            String input;
 
+            input = scanner.next();
+
+
+            if (input.equalsIgnoreCase("quit")) {
+                break;
+            }
+            inputCorrect(input,num1,num2);
+
+
+        }
 
 
     }
 
+    public static void inputCorrect (String input,int num1,int num2) {
+    try {
+        if (Integer.parseInt(input) == num1 + num2) {
+            System.out.println("correct");
+        } else {
+            int result = num1 + num2;
+            System.out.println("not correct the result is " + result);
+        }
+    } catch (Exception e) {
+        System.out.println("number as input needed try again");
+        Scanner scanner = new Scanner(System.in);
+        String userInput = scanner.next();
+        if (userInput.equalsIgnoreCase("quit")) {
+            return;
+        }
+        inputCorrect(userInput,num1,num2);
+    }
+
+    }
 
 
 

77ec57d4fa9e2f35ef94950679a7ef262d9c8f8a
therandomdud <mail@mymail.com>
2023-01-07 22:16:31 +0100

    add game loop

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,35 @@
+import java.util.Locale;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        boolean runner = true;
+        Scanner scanner = new Scanner(System.in);
+
+        while (runner) {
+            System.out.println("Type start to yes or no to quit");
+            String userInput = scanner.next();
+            if (userInput.equalsIgnoreCase("yes")) {
+                mathGame();
+            }
+            else {
+                System.out.println("bye");
+                runner = false;
+            }
+
+        }
+    }
+
+    public static void mathGame () {
+
+
 
     }
+
+
+
+
+
 }
